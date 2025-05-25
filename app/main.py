6201e0846506d2498f89583cab12c9edfec26edf
specture258 <specture258@hanyang.ac.kr>
2025-05-26 02:25:11 +0900

    Test:

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.api.routes import router
 from app.core.config import get_settings
-from prometheus_fastapi_instrumentator import Instrumentator
+
 import os
 
 settings = get_settings()
@@ -12,7 +12,7 @@ app = FastAPI(
     description="뉴스 기사를 3줄로 요약해주는 API",
     version=settings.APP_VERSION
 )
-Instrumentator().instrument(app).expose(app)
+
 #####
 #정적 파일 설정 (테스트 환경이 아닐 때만)
 if not os.getenv("TESTING"):
