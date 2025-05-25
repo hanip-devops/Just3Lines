2115d10826a55247e820027c3857f3e51851d2d4
specture258 <specture258@hanyang.ac.kr>
2025-05-26 01:00:39 +0900

    Test:

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ app = FastAPI(
     version=settings.APP_VERSION
 )
 
-정적 파일 설정 (테스트 환경이 아닐 때만)
+#정적 파일 설정 (테스트 환경이 아닐 때만)
 if not os.getenv("TESTING"):
    app.mount("/static", StaticFiles(directory="static"), name="static")
 
